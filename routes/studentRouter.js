d186ca46e0cd08c72dc2abd399d321af3bfe23e0
Flavio Milani <milanidesign7@gmail.com>
2020-11-08 10:52:47 -0300

    Adicionado rota de put

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,5 +1,19 @@
+import { studentModel } from '../models/studentModel';
+
 const express = require('express');
 
 const app = express();
 
+app.put('/student/:id', async (req, res) => {
+  try {
+    const student = await studentModel.findOneAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.send(student);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 export { app as studentRouter };
